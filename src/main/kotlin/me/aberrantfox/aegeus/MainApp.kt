8f9e12ac1e2a32d5cf7c12a4627b472ca21c3423
Fox#0001 <aberrantfoxtph@gmail.com>
2017-05-21 16:17:19 +0100

    Initial commit

---
+++ src/main/kotlin/me/aberrantfox/aegeus/MainApp.kt
@@ -0,0 +1,11 @@
+package me.aberrantfox.aegeus
+
+data class TestJson(val id: Int, val name: String)
+
+fun main(args: Array<String>) {
+    println("Starting to load Aegeus bot.")
+}
+
+fun produceBot() = {
+
+}
