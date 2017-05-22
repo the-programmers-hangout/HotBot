6bf02b3d55e114c51f92cf1919f819e69582704e
Fox#0001 <aberrantfoxtph@gmail.com>
2017-05-22 15:22:15 +0100

    Introduction of command map

--- src/main/kotlin/me/aberrantfox/aegeus/MainApp.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/MainApp.kt
@@ -1,11 +1,21 @@
 package me.aberrantfox.aegeus
 
-data class TestJson(val id: Int, val name: String)
+import me.aberrantfox.aegeus.businessobjects.Configuration
+import me.aberrantfox.aegeus.businessobjects.produceConfigOrFail
+import me.aberrantfox.aegeus.commandframework.produceCommandMap
+import me.aberrantfox.aegeus.listeners.CommandListener
+import net.dv8tion.jda.core.AccountType
+import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.core.JDABuilder
+
 
 fun main(args: Array<String>) {
     println("Starting to load Aegeus bot.")
-}
 
-fun produceBot() = {
+    val commandMap = produceCommandMap()
+    val config: Configuration = produceConfigOrFail(commandMap)
 
+    val jda: JDA = JDABuilder(AccountType.BOT).setToken(config.token).buildBlocking()
+
+    jda.addEventListener(CommandListener(jda, config, commandMap))
 }
