cfe1fbda7c1110ff5a7b1bd384f4dcb0ad0491cb
Fox#0001 <aberrantfoxtph@gmail.com>
2017-05-27 23:33:48 +0100

    Removal of redundant types

--- src/main/kotlin/me/aberrantfox/aegeus/MainApp.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/MainApp.kt
@@ -1,11 +1,9 @@
 package me.aberrantfox.aegeus
 
-import me.aberrantfox.aegeus.businessobjects.Configuration
 import me.aberrantfox.aegeus.businessobjects.produceConfigOrFail
 import me.aberrantfox.aegeus.commandframework.produceCommandMap
 import me.aberrantfox.aegeus.listeners.CommandListener
 import net.dv8tion.jda.core.AccountType
-import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
 
 
@@ -13,9 +11,8 @@ fun main(args: Array<String>) {
     println("Starting to load Aegeus bot.")
 
     val commandMap = produceCommandMap()
-    val config: Configuration = produceConfigOrFail(commandMap)
-
-    val jda: JDA = JDABuilder(AccountType.BOT).setToken(config.token).buildBlocking()
+    val config = produceConfigOrFail(commandMap)
+    val jda  = JDABuilder(AccountType.BOT).setToken(config.token).buildBlocking()
 
     jda.addEventListener(CommandListener(jda, config, commandMap))
 }
