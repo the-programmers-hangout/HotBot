9916dead3972f8bcc8eb743563bd786fe9cb4100
Fox#0001 <aberrantfoxtph@gmail.com>
2017-05-28 00:09:06 +0100

    Initial commit of set permission command

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/PermissionsCommands.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/PermissionsCommands.kt
@@ -1,12 +1,25 @@
 package me.aberrantfox.aegeus.commandframework.commands
 
 import me.aberrantfox.aegeus.businessobjects.Configuration
-import me.aberrantfox.aegeus.commandframework.ArgumentType
-import me.aberrantfox.aegeus.commandframework.Command
+import me.aberrantfox.aegeus.commandframework.*
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 
-//setPerm command newperm
 @Command(ArgumentType.STRING, ArgumentType.STRING)
-fun setPerm(event: MessageReceivedEvent, args: List<String>,  config: Configuration) {
+fun setPerm(event: MessageReceivedEvent, args: List<Any>,  config: Configuration) {
+    val commandName = args[0] as String
+    val desiredPermission = args[1] as String
+    
+    if( !(config.commandPermissionMap.contains(commandName)) ) {
+        event.channel.sendMessage("Dunno what the command:$commandName is - run the help command?").queue()
+        return
+    }
 
+    val permission = stringToPermission(desiredPermission)
+
+    if (permission == null) {
+        event.channel.sendMessage("Yup. That permission level doesn't exist. Sorry").queue()
+        return
+    }
+
+    config.commandPermissionMap[commandName] = permission
 }
