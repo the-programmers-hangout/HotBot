5b63b571e1cf9b6e13b4520495edbf034f9bc9ce
Fox#0001 <aberrantfoxtph@gmail.com>
2017-05-28 00:43:21 +0100

    addition of exit command

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/GeneralCommands.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/GeneralCommands.kt
@@ -1,5 +1,7 @@
 package me.aberrantfox.aegeus.commandframework.commands
 
+import me.aberrantfox.aegeus.businessobjects.Configuration
+import me.aberrantfox.aegeus.businessobjects.saveConfigurationFile
 import me.aberrantfox.aegeus.commandframework.ArgumentType
 import me.aberrantfox.aegeus.commandframework.Command
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
@@ -11,8 +13,12 @@ val startTime = Date()
 
 @Command fun help(event: MessageReceivedEvent) = event.channel.sendMessage("url-to-help-to-be-added").queue()
 
-@Command fun uptime(event: MessageReceivedEvent) = event.channel.sendMessage("I've been awake since " +
-        "${startTime.toString()}, so like... ${(Date().time - startTime.time) / 1000 / 60} minutes").queue()
+@Command fun uptime(event: MessageReceivedEvent) {
+    val minutes = Date().time - startTime.time / 1000 / 60
+    val currentDate = startTime.toString()
+
+    event.channel.sendMessage("I've been awake since ${currentDate}, so like... ${minutes} minutes").queue()
+}
 
 @Command(ArgumentType.INTEGER, ArgumentType.INTEGER)
 fun add(event: MessageReceivedEvent, args: List<Any>) {
@@ -20,4 +26,17 @@ fun add(event: MessageReceivedEvent, args: List<Any>) {
     val right = args[1] as Int
 
     event.channel.sendMessage("Result: ${left + right}").queue()
+}
+
+@Command(ArgumentType.STRING)
+fun exit(event: MessageReceivedEvent, args: List<Any>, config: Configuration) {
+    val saveConfiguration = args[0] as String
+
+    if(saveConfiguration.toLowerCase() == "true") {
+        saveConfigurationFile(config)
+    }
+
+    event.channel.sendMessage("Exiting").queue()
+
+    System.exit(0)
 }
