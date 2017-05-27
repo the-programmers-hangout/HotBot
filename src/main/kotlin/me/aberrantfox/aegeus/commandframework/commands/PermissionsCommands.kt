151680f75be868865d075629ce038c53c1807e9e
Fox#0001 <aberrantfoxtph@gmail.com>
2017-05-27 23:36:32 +0100

    Addition of argument typings

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/PermissionsCommands.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/PermissionsCommands.kt
@@ -1,8 +1,12 @@
 package me.aberrantfox.aegeus.commandframework.commands
 
+import me.aberrantfox.aegeus.businessobjects.Configuration
+import me.aberrantfox.aegeus.commandframework.ArgumentType
 import me.aberrantfox.aegeus.commandframework.Command
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 
-
-@Command fun setPermission() {
+//setPerm command newperm
+@Command(ArgumentType.STRING, ArgumentType.STRING)
+fun setPerm(event: MessageReceivedEvent, args: List<String>,  config: Configuration) {
 
 }
