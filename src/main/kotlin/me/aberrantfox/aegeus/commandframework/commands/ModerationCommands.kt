095929fce69b4b0a47c7709d213b63ca59f43582
Fox#0001 <aberrantfoxtph@gmail.com>
2017-06-03 14:23:35 +0100

    initial commit of ignore channels command

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/ModerationCommands.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/ModerationCommands.kt
@@ -2,7 +2,9 @@ package me.aberrantfox.aegeus.commandframework.commands
 
 import me.aberrantfox.aegeus.commandframework.ArgumentType
 import me.aberrantfox.aegeus.commandframework.Command
+import me.aberrantfox.aegeus.services.Configuration
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+
 
 @Command(ArgumentType.INTEGER)
 fun nuke(event: MessageReceivedEvent, args: List<Any>) {
@@ -17,4 +19,20 @@ fun nuke(event: MessageReceivedEvent, args: List<Any>) {
         it.forEach { it.delete().queue() }
         event.channel.sendMessage("Be nice. No spam.").queue()
     })
+}
+
+@Command(ArgumentType.STRING)
+fun ignore(event: MessageReceivedEvent, args: List<Any>, config: Configuration) {
+    val target = args[0] as String
+
+    println(event.channel.name)
+
+    if(config.ignoredChannels.contains(target)) {
+        config.ignoredChannels.remove(target)
+        event.channel.sendMessage("Now accepting commands in #$target again.").queue()
+    } else {
+        config.ignoredChannels.add(target)
+        event.channel.sendMessage("I will no longer accept commands from #$target.").queue()
+    }
+
 }
