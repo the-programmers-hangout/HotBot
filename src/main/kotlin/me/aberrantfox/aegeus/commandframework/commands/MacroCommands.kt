12f05a2d896d54cf7d53bb23df6afedc98f0e2b5
Fox#0001 <aberrantfoxtph@gmail.com>
2017-08-06 16:39:19 +0100

    addition of macro commands

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/MacroCommands.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/MacroCommands.kt
@@ -2,13 +2,15 @@ package me.aberrantfox.aegeus.commandframework.commands
 
 import com.google.common.reflect.TypeToken
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import me.aberrantfox.aegeus.commandframework.ArgumentType
 import me.aberrantfox.aegeus.commandframework.Command
 import me.aberrantfox.aegeus.services.Configuration
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 import java.io.File
 
-val macroMap = loadMacroMap()
+private val macroMap = loadMacroMap()
+private val mapLocation = "macros.json"
 
 @Command(ArgumentType.Manual)
 fun addMacro(event: GuildMessageReceivedEvent, args: List<Any>, config: Configuration) {
@@ -21,26 +23,41 @@ fun addMacro(event: GuildMessageReceivedEvent, args: List<Any>, config: Configur
 }
 
 @Command(ArgumentType.String)
-fun removeMacro() {
-    
+fun removeMacro(event: GuildMessageReceivedEvent, args: List<Any>) {
+    val key = (args[0] as String).toLowerCase()
+
+    if(macroMap.containsKey(key)) {
+        macroMap.remove(key)
+        saveMacroMap(macroMap)
+        event.channel.sendMessage("$key - this macro is now gone.").queue()
+        return
+    }
+
+    event.channel.sendMessage("$key isn't a macro... ").queue()
 }
 
-private val gson = Gson()
-private val mapLocation = "macros.json"
+@Command
+fun listMacros(event: GuildMessageReceivedEvent) {
+    val macros = macroMap.keys.reduce { acc, s -> "$acc, $s" }
+    event.channel.sendMessage("Currently available macros: $macros.").queue()
+}
 
 private fun loadMacroMap(): HashMap<String, String> {
-    val map: HashMap<String, String> = HashMap()
     val file = File(mapLocation)
+    val gson = Gson()
 
     if( !(file.exists()) ) {
-        return map
+        return HashMap()
     }
-    val type = object : TypeToken<HashMap<String, String>>() {}.type
 
-    return gson.fromJson(file.readText(), type)
+    val type = object : TypeToken<HashMap<String, String>>() {}.type
+    val map = gson.fromJson<HashMap<String, String>>(file.readText(), type)
+
+    return map
 }
 
-fun saveMacroMap(map: HashMap<String, String>) {
+private fun saveMacroMap(map: HashMap<String, String>) {
+    val gson = Gson()
     val json = gson.toJson(map)
     val file = File(mapLocation)
 
