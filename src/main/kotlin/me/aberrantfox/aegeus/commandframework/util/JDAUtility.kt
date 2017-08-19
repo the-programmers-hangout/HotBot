e0fefc27dccb15d9a09100e4a938b1a414f0573b
Fox#0001 <aberrantfoxtph@gmail.com>
2017-08-19 21:13:21 +0100

    Creation of mute/unmute functions and corresponding time-text extension function

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/util/JDAUtility.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/util/JDAUtility.kt
@@ -1,7 +1,10 @@
 package me.aberrantfox.aegeus.commandframework.util
 
+import me.aberrantfox.aegeus.services.Configuration
 import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.entities.User
+import java.util.*
 
 
 fun String.isUserID(jda: JDA): Boolean = jda.getUserById(this) != null
@@ -9,3 +12,34 @@ fun String.isUserID(jda: JDA): Boolean = jda.getUserById(this) != null
 fun String.idToName(jda: JDA): String = jda.getUserById(this).name
 
 fun String.idToUser(jda: JDA): User = jda.getUserById(this)
+
+fun Guild.hasRole(roleName: String): Boolean = this.roles.any { it.name.toLowerCase() == roleName }
+
+fun Long.convertToTimeString(): String {
+    val seconds = this / 1000
+    val minutes = seconds / 60
+    val hours = minutes / 60
+    val days = hours / 24
+
+    return "$days:${hours % 24}:${minutes % 60}:${seconds % 60}"
+}
+
+fun muteMember(guild: Guild, user: User, time: Long, reason: String, config: Configuration) {
+    guild.controller.addRolesToMember(guild.getMemberById(user.id), guild.getRolesByName(config.mutedRole, true)).queue()
+
+    user.openPrivateChannel().queue {
+        it.sendMessage("You have been muted for ${time.convertToTimeString()} minutes, reason: $reason").queue()
+        unmute(guild, user, config, time)
+    }
+}
+
+fun unmute(guild: Guild, user: User, config: Configuration, time: Long) =
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                guild.controller.removeRolesFromMember(guild.getMemberById(user.id), guild.getRolesByName(config.mutedRole, true)).queue()
+                user.openPrivateChannel().queue {
+                    it.sendMessage("${user.name} - you have been unmuted. Please respect our rules to prevent" +
+                        " further infractions.").queue()
+                }
+            }
+        }, time)
