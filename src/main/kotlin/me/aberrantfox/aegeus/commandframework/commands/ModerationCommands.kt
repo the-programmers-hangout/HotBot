a575e2b157eb3b4c997ceda68f3f2c77352080c9
Fox#0001 <aberrantfoxtph@gmail.com>
2017-08-19 21:05:41 +0100

    Refactoring of mute command into its own seperate functionality

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/ModerationCommands.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/ModerationCommands.kt
@@ -2,15 +2,16 @@ package me.aberrantfox.aegeus.commandframework.commands
 
 import me.aberrantfox.aegeus.commandframework.ArgumentType
 import me.aberrantfox.aegeus.commandframework.Command
-import me.aberrantfox.aegeus.commandframework.Permission
 import me.aberrantfox.aegeus.commandframework.stringToPermission
+import me.aberrantfox.aegeus.commandframework.util.idToUser
+import me.aberrantfox.aegeus.commandframework.util.muteMember
+import me.aberrantfox.aegeus.commandframework.util.unmute
 import me.aberrantfox.aegeus.services.Configuration
 import net.dv8tion.jda.core.OnlineStatus
 import net.dv8tion.jda.core.entities.Game
 import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.entities.TextChannel
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
-import java.util.*
 
 
 @Command(ArgumentType.Integer)
@@ -41,26 +42,14 @@ fun ignore(event: GuildMessageReceivedEvent, args: List<Any>, config: Configurat
     }
 }
 
-@Command(ArgumentType.Integer, ArgumentType.String)
+@Command(ArgumentType.UserID, ArgumentType.Integer, ArgumentType.Joiner)
 fun mute(event: GuildMessageReceivedEvent, args: List<Any>, config: Configuration) {
-    val minutes = args[0] as Int
-    val target = args[1] as String
-    val targetID = event.message.mentionedUsers.map { it.id }.first()
-    val time = minutes.toLong() * 1000 * 60
+    val user = (args[0] as String).idToUser(event.jda)
+    val time = (args[1] as Int).toLong() * 1000 * 60
+    val reason = args[2] as String
+    val guild = event.guild
 
-    config.mutedMembers.add(targetID)
-    event.channel.sendMessage("$target is now muted for $minutes minutes").queue()
-
-    println(targetID)
-
-    val timer = Timer()
-    timer.schedule(object : TimerTask() {
-        override fun run() {
-            config.mutedMembers.remove(targetID)
-            event.channel.sendMessage("$target - you have been unmuted. Please respect our rules to prevent" +
-                    " further infractions.").queue()
-        }
-    }, time)
+    muteMember(guild, user, time, reason, config)
 }
 
 @Command
