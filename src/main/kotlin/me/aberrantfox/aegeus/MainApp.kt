645c6d803d4ab5cd8d8f7a45f7f5ad593eb124e5
Fox#0001 <aberrantfoxtph@gmail.com>
2017-08-19 23:28:32 +0100

    refactoring

--- src/main/kotlin/me/aberrantfox/aegeus/MainApp.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/MainApp.kt
@@ -11,8 +11,6 @@ import net.dv8tion.jda.core.OnlineStatus
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.Game
 import net.dv8tion.jda.core.entities.Guild
-import net.dv8tion.jda.core.entities.TextChannel
-import java.util.*
 
 
 fun main(args: Array<String>) {
