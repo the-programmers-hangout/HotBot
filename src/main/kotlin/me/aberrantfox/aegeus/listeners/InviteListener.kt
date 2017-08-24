46ac797ae7528df85f6e78dfd5ecef22af59e924
Fox#0001 <aberrantfoxtph@gmail.com>
2017-08-24 23:01:53 +0100

    Enforced regex on message edits

--- src/main/kotlin/me/aberrantfox/aegeus/listeners/InviteListener.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/listeners/InviteListener.kt
@@ -2,18 +2,23 @@ package me.aberrantfox.aegeus.listeners
 
 import me.aberrantfox.aegeus.services.Configuration
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.core.events.message.guild.GuildMessageUpdateEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 
 
 class InviteListener(val config: Configuration) : ListenerAdapter() {
-    override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
+    override fun onGuildMessageUpdate(event: GuildMessageUpdateEvent) = handlePossibleInviteMessage(event)
+
+    override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) = handlePossibleInviteMessage(event)
+
+    private fun handlePossibleInviteMessage(event: GuildMessageReceivedEvent) {
         if(event.author.isBot) return
 
         if(event.message.rawContent.matches(Regex("(.|\n)*(https://discord.gg/)+(.|\n)*"))) {
 
             if(event.message.rawContent.contains('@'))
 
-            event.message.delete().queue()
+                event.message.delete().queue()
             event.guild.textChannels.findLast { it.id == config.leaveChannel }
                     ?.sendMessage("Deleted message: ${event.message.rawContent} " +
                             "by ${event.author.asMention} " +
