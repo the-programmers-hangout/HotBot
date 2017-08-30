89c203242ca11b06b0e523eda83132b4a7c0e38a
Fox#0001 <aberrantfoxtph@gmail.com>
2017-08-30 21:55:46 +0100

    Addition of means to pull suggestion row from database

--- src/main/kotlin/me/aberrantfox/aegeus/services/database/SuggestionTransactions.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/services/database/SuggestionTransactions.kt
@@ -2,33 +2,48 @@ package me.aberrantfox.aegeus.services.database
 
 import me.aberrantfox.aegeus.commandframework.commands.Suggestion
 import me.aberrantfox.aegeus.commandframework.commands.SuggestionStatus
-import org.jetbrains.exposed.sql.Op
-import org.jetbrains.exposed.sql.deleteWhere
-import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.update
 
 
-fun trackSuggestion(suggestion: Suggestion, status: SuggestionStatus) =
+fun trackSuggestion(suggestion: Suggestion, status: SuggestionStatus, messageID: String) =
         transaction {
             Suggestions.insert {
+                it[Suggestions.id] = messageID
                 it[Suggestions.date] = suggestion.timeOf
                 it[Suggestions.idea] = suggestion.idea
                 it[Suggestions.member] = suggestion.member
                 it[Suggestions.status] = status
+                it[Suggestions.avatarURL] = suggestion.avatarURL
             }
         }
 
-fun updateSuggestion(id: Int, status: SuggestionStatus) =
+fun updateSuggestion(id: String, status: SuggestionStatus) =
         transaction {
             Suggestions.update ({Suggestions.id eq id }) {
                 it[Suggestions.status] = status
             }
         }
 
-fun deleteSuggestion(id: Int) =
+fun deleteSuggestion(member: String, idea: String) =
         transaction {
             Suggestions.deleteWhere {
-                Op.build { Suggestions.id eq id }
+                Op.build { (Suggestions.member eq member) and (Suggestions.idea eq idea) }
             }
+        }
+
+fun obtainSuggestion(id: String) =
+        transaction {
+            val row = Suggestions.select {
+                Op.build { Suggestions.id eq id }
+            }.first()
+
+            Suggestion(row[Suggestions.member], row[Suggestions.idea], row[Suggestions.date], row[Suggestions.avatarURL])
+        }
+
+fun isTracked(id: String) =
+        transaction {
+            Suggestions.select {
+                Op.build { Suggestions.id eq id }
+            }.count() > 0
         }
