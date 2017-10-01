d9c64525edeb4764582c95d6aa7838038376edd8
Fox#0001 <aberrantfoxtph@gmail.com>
2017-10-01 15:45:35 +0100

    documentation of echo and version fix

--- src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/OwnerCommands.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/OwnerCommands.kt
@@ -8,7 +8,7 @@ import me.aberrantfox.aegeus.commandframework.CommandEvent
 @Command(ArgumentType.String, ArgumentType.Joiner)
 fun echo(event: CommandEvent) {
     val target = event.args[0] as String
-    val message = event.args[0] as String
+    val message = event.args[1] as String
 
     event.jda.getTextChannelById(target).sendMessage(message).queue()
 }
