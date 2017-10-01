c0742718b1d9e64cb705520fc340c8de606912a9
Fox#0001 <aberrantfoxtph@gmail.com>
2017-10-01 15:41:24 +0100

    Addition of echo command

---
+++ src/main/kotlin/me/aberrantfox/aegeus/commandframework/commands/OwnerCommands.kt
@@ -0,0 +1,14 @@
+package me.aberrantfox.aegeus.commandframework.commands
+
+import me.aberrantfox.aegeus.commandframework.ArgumentType
+import me.aberrantfox.aegeus.commandframework.Command
+import me.aberrantfox.aegeus.commandframework.CommandEvent
+
+
+@Command(ArgumentType.String, ArgumentType.Joiner)
+fun echo(event: CommandEvent) {
+    val target = event.args[0] as String
+    val message = event.args[0] as String
+
+    event.jda.getTextChannelById(target).sendMessage(message).queue()
+}
