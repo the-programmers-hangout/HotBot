69cdede3f6057b4039a88b43f9a2db26edd7bf50
Fox#0001 <aberrantfoxtph@gmail.com>
2017-11-26 01:24:47 +0000

    Swap to junit

---
+++ src/test/kotlin/me/aberrantfox/aegus/extenstions/IniviteMatchTest.kt
@@ -0,0 +1,28 @@
+package me.aberrantfox.aegus.extenstions
+
+import me.aberrantfox.aegeus.extensions.containsInvite
+import net.dv8tion.jda.core.MessageBuilder
+import org.junit.Assert
+import org.junit.Test
+
+class InviteMatchTest {
+    @Test
+    fun invitesMatchValidInvites() = Assert.assertTrue(msg("https://discord.gg/BQN6BYE").containsInvite())
+
+    @Test
+    fun doesntMatchNonInvite() = Assert.assertFalse(msg("aaa bbb ccc 123aosd").containsInvite())
+
+    @Test
+    fun matchesMultipleInvites() =
+        Assert.assertTrue(msg("https://discord.gg/BQN6BYE https://discord.gg/Basdas").containsInvite())
+
+    @Test
+    fun matchesInvitesWithNewLines() =
+        Assert.assertTrue(msg("\n\nhttps://discord.gg/BQN6BYE \n \n https://discord.gg/BQN6BYE").containsInvite())
+
+    @Test
+    fun matchesCommandInvocation() =
+        Assert.assertTrue(msg("\$https://discord.gg/sfwe34jd").containsInvite())
+}
+
+fun msg(data: String) = MessageBuilder().append(data).build()
---
+++ src/test/kotlin/me/aberrantfox/aegus/security/NewPlayerTrackerTest.kt
@@ -0,0 +1,31 @@
+package me.aberrantfox.aegus.security
+
+import me.aberrantfox.aegeus.services.security.NewPlayers
+import org.joda.time.DateTime
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+
+class NewPlayerTrackerTest {
+
+    @Before
+    fun beforeEach() = NewPlayers.cache.clear()
+
+    @Test
+    fun aPlayerJoiningShouldBeTracked() {
+        NewPlayers.cache.put("a", DateTime.now())
+        NewPlayers.cache.put("b", DateTime.now())
+        NewPlayers.cache.put("c", DateTime.now())
+        NewPlayers.cache.put("d", DateTime.now())
+
+        Assert.assertEquals(4, NewPlayers.pastMins(1).size)
+    }
+
+    @Test
+    fun playersJoiningOutOfScopeShouldntBeListed() {
+        NewPlayers.cache.put("a", DateTime.now())
+        Thread.sleep(100)
+        Assert.assertEquals(0, NewPlayers.pastMins(0).size)
+    }
+}
