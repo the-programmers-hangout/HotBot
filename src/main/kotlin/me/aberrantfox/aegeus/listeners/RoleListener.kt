7ee9d3a643f2dbb1326ff0bf45b9b98438879913
Joshua Thelwall <joshuathelwall@hotmail.co.uk>
2018-01-01 11:46:07 +0000

    Setup RoleListener

---
+++ src/main/kotlin/me/aberrantfox/aegeus/listeners/RoleListener.kt
@@ -0,0 +1,13 @@
+package me.aberrantfox.aegeus.listeners
+
+import net.dv8tion.jda.core.hooks.ListenerAdapter
+import net.dv8tion.jda.core.events.role.update.RoleUpdateNameEvent
+import me.aberrantfox.aegeus.services.Configuration
+import me.aberrantfox.aegeus.commandframework.commands.RankContainer
+
+class RoleListener(val configuration: Configuration) : ListenerAdapter() {
+    override fun onRoleUpdateName(event: RoleUpdateNameEvent) {
+        val oldName = event.oldName
+        val newName = event.role.name
+    }
+}
