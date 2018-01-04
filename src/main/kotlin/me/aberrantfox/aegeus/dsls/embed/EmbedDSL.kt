138d00c79524863d7894e483c863f23dbae11b1a
Fox#0001 <aberrantfoxtph@gmail.com>
2018-01-04 17:12:30 +0000

    Implementation of embed DSL for nicer embed construction

---
+++ src/main/kotlin/me/aberrantfox/aegeus/dsls/embed/EmbedDSL.kt
@@ -0,0 +1,33 @@
+package me.aberrantfox.aegeus.dsls.embed
+
+import net.dv8tion.jda.core.EmbedBuilder
+import net.dv8tion.jda.core.entities.MessageEmbed
+
+
+class EmbedDSLHandle : EmbedBuilder() {
+    operator fun invoke(args: EmbedDSLHandle.() -> Unit) {}
+
+    fun title(title: String) = this.setTitle(title)
+
+    fun description(descr: String) = this.setDescription(descr)
+
+    fun field(construct: FieldStore.() -> Unit) {
+        val field = FieldStore()
+        field.construct()
+        addField(field.name, field.value, field.inline)
+    }
+
+    fun ifield(construct: FieldStore.() -> Unit) {
+        val field = FieldStore()
+        field.construct()
+        addField(field.name, field.value, true)
+    }
+}
+
+data class FieldStore(var name: String = "", var value: String = "", var inline: Boolean = true)
+
+fun embed(construct: EmbedDSLHandle.() -> Unit): MessageEmbed {
+    val handle = EmbedDSLHandle()
+    handle.construct()
+    return handle.build()
+}
