26a2360ca11c1217ebd2f9533cf212aa3c1f9424
Fox#0001 <aberrantfoxtph@gmail.com>
2018-01-10 15:52:16 +0000

    Move to new package and creation of permissionstransactions

---
+++ src/main/kotlin/me/aberrantfox/aegeus/database/PermissionsTransactions.kt
@@ -0,0 +1,3 @@
+package me.aberrantfox.aegeus.database
+
+
---
+++ src/main/kotlin/me/aberrantfox/aegeus/database/SetupOperations.kt
@@ -0,0 +1,57 @@
+package me.aberrantfox.aegeus.database
+
+import me.aberrantfox.aegeus.commandframework.commands.SuggestionStatus
+import me.aberrantfox.aegeus.services.Configuration
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun setupDatabaseSchema(config: Configuration) {
+    val dbParams = listOf(
+            "useUnicode=true",
+            "useJDBCCompliantTimezoneShift=true",
+            "useLegacyDatetimeCode=true",
+            "serverTimezone=UTC",
+            "nullNamePatternMatchesAll=true"
+    )
+
+    val url = "jdbc:mysql://${config.databaseCredentials.hostname}/${config.databaseCredentials.database}?${dbParams.joinToString("&")}"
+
+    Database.connect(
+            url = url,
+            driver = "com.mysql.cj.jdbc.Driver",
+            password = config.databaseCredentials.password,
+            user = config.databaseCredentials.username
+    )
+
+    transaction {
+        SchemaUtils.create(Strikes, Suggestions, BanRecords)
+        logger.addLogger(StdOutSqlLogger)
+    }
+}
+
+object Strikes : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val moderator = varchar("moderator", 18)
+    val member = varchar("member", 18)
+    val strikes = integer("strikes")
+    val reason = text("reason")
+    val date = date("date")
+}
+
+object Suggestions : Table() {
+    val id = varchar("id", 18).primaryKey()
+    val avatarURL = varchar("url", 1024)
+    val member = varchar("member", 18)
+    val status = enumeration("status", SuggestionStatus::class.java)
+    val idea = text("idea")
+    val date = date("date")
+}
+
+object BanRecords : Table() {
+    val id = varchar("id", 18).primaryKey()
+    val reason = text("reason")
+    val moderator = varchar("moderator", 18)
+}
