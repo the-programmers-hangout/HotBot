8c7aa3037c6936a21c75f244a2fefd751443f819
Fox#0001 <aberrantfoxtph@gmail.com>
2018-01-12 00:12:33 +0000

    Addition of elvis check to handle someone with just @everyone

--- src/main/kotlin/me/aberrantfox/aegeus/extensions/GuildMemberExtensions.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/extensions/GuildMemberExtensions.kt
@@ -3,4 +3,4 @@ package me.aberrantfox.aegeus.extensions
 import net.dv8tion.jda.core.entities.Member
 
 
-fun Member.getHighestRole() = roles.maxBy { it.position }
+fun Member.getHighestRole() = roles.maxBy { it.position } ?: -1
