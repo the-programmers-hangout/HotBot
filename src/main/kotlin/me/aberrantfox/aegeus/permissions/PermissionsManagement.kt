1806e5038b47b7167d1522e802f71f92b36a1d03
Fox#0001 <aberrantfoxtph@gmail.com>
2018-01-12 00:07:39 +0000

    Update to manager to maintain role-hierarchy based permission set

--- src/main/kotlin/me/aberrantfox/aegeus/permissions/PermissionsManagement.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/permissions/PermissionsManagement.kt
@@ -1,16 +1,37 @@
 package me.aberrantfox.aegeus.permissions
 
-import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.entities.Role
 
+typealias RoleID = String
+typealias CommandName = String
 
-class RoleContainer(private val roleMap: HashMap<String, Pair<Role, Boolean>> = HashMap(),
-                    private val commandMap: HashMap<String, String> = HashMap()) {
-    fun addRole(id: String, role: Role) = roleMap.put(id, Pair(role, false))
+data class PermissionManager(val map: HashMap<RoleID, HashSet<CommandName>> = HashMap(), private val roles: List<Role>) {
 
-    fun removeRole(id: String) = roleMap.remove(id)
+    fun addPermission(roleID: RoleID, name: CommandName) =
+        if(map.containsKey(roleID)) {
+            map.get(roleID)!!.add(name)
+        } else {
+            map.put(roleID, hashSetOf(name))
+        }
 
-    fun getRole(id: String) = roleMap[id]
+    fun hasPermission(roleID: RoleID, commandName: CommandName) =
+        getAllRelevantRoleIds(roleID)
+            .filter { map.containsKey(it) }
+            .any { map[it]!!.contains(commandName) }
 
-    fun listRoles() = roleMap.keys
+    fun listAvailableCommands(roleID: RoleID) = getAllRelevantRoleIds(roleID)
+        .map { map.get(it) }
+        .reduceRight { a, b -> a!!.addAll(b!!) ; a }!!
+
+    private fun getAllRelevantRoleIds(roleID: RoleID): List<String> {
+        val role = roles.first { it.id == roleID }
+        val lowerRoles = ArrayList(roles
+            .filter { it.position < role.position }
+            .map { it.id }
+            .toList())
+
+        lowerRoles.add(roleID)
+
+        return lowerRoles.filter {map.containsKey(it)}
+    }
 }
