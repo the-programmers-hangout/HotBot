975579bb07de0b42534c579a2d0d1f670d36ab6c
Fox#0001 <aberrantfoxtph@gmail.com>
2018-01-19 22:07:48 +0000

    Filtered messages to new channel logs

--- src/main/kotlin/me/aberrantfox/aegeus/listeners/VoiceChannelListener.kt
+++ src/main/kotlin/me/aberrantfox/aegeus/listeners/VoiceChannelListener.kt
@@ -1,20 +1,20 @@
 package me.aberrantfox.aegeus.listeners
 
 import me.aberrantfox.aegeus.extensions.descriptor
-import net.dv8tion.jda.core.entities.MessageChannel
+import me.aberrantfox.aegeus.logging.BotLogger
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 
 
-class VoiceChannelListener (val channel: MessageChannel) : ListenerAdapter() {
+class VoiceChannelListener (val log: BotLogger) : ListenerAdapter() {
     override fun onGuildVoiceJoin(event: GuildVoiceJoinEvent) =
-        channel.sendMessage("**Voice Join** ${event.member.descriptor()} :: ${event.channelJoined.name}").queue()
+        log.voice("**Voice Join** ${event.member.descriptor()} :: ${event.channelJoined.name}")
 
     override fun onGuildVoiceLeave(event: GuildVoiceLeaveEvent) =
-        channel.sendMessage("**Voice Leave** ${event.member.descriptor()} :: ${event.channelLeft.name}").queue()
+        log.voice("**Voice Leave** ${event.member.descriptor()} :: ${event.channelLeft.name}")
 
     override fun onGuildVoiceMove(event: GuildVoiceMoveEvent) =
-        channel.sendMessage("**Voice Move** ${event.member.descriptor()} :: ${event.channelJoined.name}").queue()
+        log.voice("**Voice Move** ${event.member.descriptor()} :: ${event.channelJoined.name}")
 }
