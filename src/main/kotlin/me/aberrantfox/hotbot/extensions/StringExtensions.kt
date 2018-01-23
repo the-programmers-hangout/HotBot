7207e95009efd1b0134ba00a934b07c695d6998c
Fox#0001 <aberrantfoxtph@gmail.com>
2018-01-23 19:14:35 +0000

    Implemented a very simple formatter for discord dates

--- src/main/kotlin/me/aberrantfox/hotbot/extensions/StringExtensions.kt
+++ src/main/kotlin/me/aberrantfox/hotbot/extensions/StringExtensions.kt
@@ -1,16 +1,3 @@
 package me.aberrantfox.hotbot.extensions
 
-
-fun String.isSingleCharacter() = "^(.)\\1+\$".toRegex().matches(this)
-
-fun String.isEmojiOnly() =
-    if(this.contains(" ")) {
-        val split = this.split(" ")
-        split.all { it.wordIsEmoji() }
-    } else {
-        this.wordIsEmoji()
-    }
-
-
-fun String.wordIsEmoji() = this.startsWith(":") && this.endsWith(":")
-    || "([\\\\u20a0-\\\\u32ff\\\\ud83c\\\\udc00-\\\\ud83d\\\\udeff\\\\udbb9\\\\udce5-\\\\udbb9\\\\udcee])".toRegex().matches(this)
+fun String.formatJdaDate() = this.substring(0, this.indexOf("T"))
