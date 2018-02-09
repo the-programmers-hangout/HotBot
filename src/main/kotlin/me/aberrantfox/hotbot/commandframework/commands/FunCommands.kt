fd15259460ad7c2b1e24719219ac48d0bc4a8570
emiel <emielvdbrink@gmail.com>
2018-02-09 12:55:16 +0100

    added initial implementation of cowsay

--- src/main/kotlin/me/aberrantfox/hotbot/commandframework/commands/FunCommands.kt
+++ src/main/kotlin/me/aberrantfox/hotbot/commandframework/commands/FunCommands.kt
@@ -1,10 +1,12 @@
 package me.aberrantfox.hotbot.commandframework.commands
 
+import com.github.ricksbrown.cowsay.Cowsay
 import khttp.get as kget
 import me.aberrantfox.hotbot.commandframework.ArgumentType
 import me.aberrantfox.hotbot.commandframework.CommandSet
 import me.aberrantfox.hotbot.dsls.command.commands
 import org.jsoup.Jsoup
+import java.io.File
 import java.net.URLEncoder
 
 import java.util.*
@@ -18,7 +20,7 @@ fun funCommands() =
                 it.respond(json.getString("file"))
             }
         }
-        
+
         command("bird") {
             execute {
                 val json = kget("https://birdsare.cool/bird.json?exclude=webm,mp4").jsonObject
@@ -53,4 +55,63 @@ fun funCommands() =
                 it.respond(links.first().absUrl("href"))
             }
         }
+
+        command("cowsay") {
+            expect(ArgumentType.Sentence)
+            execute {
+                val sentence = it.args[0] as String
+
+                val response = parseCowsayArgs(sentence.split(" "))
+                if(!response.isBlank()){
+                    it.respond(response)
+                    return@execute
+                }
+
+                val args = sentence.replace("\n", " ")
+                        .split(" ")
+                        .toTypedArray()
+
+                val result = Cowsay.say(args)
+
+                if(!result.isBlank()){
+                    val response = Cowsay.say(args)
+                    if (response.length > 1994){
+                        it.respond("that message was too long, moo!")
+                        return@execute
+                    }
+
+                    it.respond("```" + Cowsay.say(args) + "```")
+                }
+            }
+        }
     }
+
+
+private fun parseCowsayArgs(arguments: List<String>): String {
+    val flagsWithArgs = Regex("-T|-W|-f|-e|--alt|--lang")
+    val flagsWithNoArgs = Regex("-b|-d|-g|-l|-n|-p|-s|-t|-w|-y")
+
+    if(!arguments.mapIndexedNotNull{index, s -> if (flagsWithArgs.matches(s)) index + 1 else null }
+            .all { it < arguments.size && !arguments[it].startsWith("-") }){
+        return "one of your flags is missing an argument"
+    }
+
+    var skipNextArg = false
+    arguments.forEach{
+        if (!skipNextArg) {
+            if(flagsWithArgs.matches(it)){
+                skipNextArg = true
+            }
+            if(it == "-h"){
+                return "```" + Scanner(File("cowsayhelp.txt")).useDelimiter("\\Z").next() + "```"
+            }
+            if(!flagsWithArgs.matches(it) && !flagsWithNoArgs.matches(it)){
+                return ""
+            }
+        }
+        else {
+            skipNextArg = false
+        }
+    }
+    return ""
+}
