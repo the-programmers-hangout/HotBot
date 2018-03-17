a4b64e13e17916f027c6e75cf27364ca68cd47ff
Moe-Szyslak <34754130+Moe-Szyslak@users.noreply.github.com>
2018-03-17 00:43:33 +0000

    Update to remove unneeded if statement
    Updated fileListener to remove unneeded if statement

--- config/scripts/custom/fileListener.js
+++ config/scripts/custom/fileListener.js
@@ -4,33 +4,29 @@
 		onGuildMessageReceived: function (event) {
 
 			var msg = event.message;
-
+			
 			if (msg.author.isBot()) { return; }
 			if (msg.member.isOwner()) { return; }
-			if (msg.attachments.length <= 0) { return; }
-
-			if (msg.attachments.length > 0) {
+			if(msg.attachments.length <=0) { return; }
 				var deletedMessage = false;
+				
 				for (var i = 0; i < msg.attachments.length; i++) {
-
 					var attachment = msg.attachments[i];
-
+					
 					if (notAllowed(attachment.fileName)) {
 						deletedMessage = true;
 						msg.delete().queue();
 						container.log.warning(msg.author.asMention + " just sent the file " +
-							attachment.getFileName() + "\n" + attachment.getUrl()
-						)
+						attachment.getFileName() + "\n" + attachment.getUrl())
 					}
 				}
 
-				if (deletedMessage) {
-					event.channel.sendMessage(
-						"Please don't send that file type here " +
-						msg.author.asMention + " use a service like " +
-						"https://hastebin.com"
-					).queue()
-				}
+			if (deletedMessage) {
+				event.channel.sendMessage(
+					"Please don't send that file type here " +
+					msg.author.asMention + " use a service like " +
+					"https://hastebin.com"
+				).queue()
 			}
 		}
 	};
