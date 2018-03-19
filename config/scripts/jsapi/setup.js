768d495d111ff3fab949ebac0f577ce4cd82096c
Aberrantfox <aberrantfoxtph@gmail.com>
2018-03-19 19:52:28 +0000

    Implementation of latex command

--- config/scripts/jsapi/setup.js
+++ config/scripts/jsapi/setup.js
@@ -1,16 +1,10 @@
 const argType = Java.type("me.aberrantfox.hotbot.commandframework.parsing.ArgumentType")
 const listener = Java.type("net.dv8tion.jda.core.hooks.ListenerAdapter")
 const EventListener = Java.extend(listener)
+const urlUtilities = Java.type("me.aberrantfox.hotbot.javautilities.UrlUtilities")
 
 function createCommand(name) {
     return container.command(name, function(){})
 }
 
-// const log = {
-//     warn: (message) => container.log.warning(message),
-//     info: (message) => container.log.info(message),
-//     cmd: (message) => container.log.cmd(message),
-//     error: (message) => container.log.error(message),
-//     voice: (message) => container.log.voice(message),
-//     history: (message) => container.log.history(message)
-// }
+const log = container.log
