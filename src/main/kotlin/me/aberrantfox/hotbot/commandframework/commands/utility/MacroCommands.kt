d75f3f95e5fcb3d347fb5fe45129d25314b15783
Joshua Thelwall <joshuathelwall@hotmail.co.uk>
2018-05-06 21:33:14 +0100

    Add new macro commands and change old ones to use Macro object
    Change addmacro and removemacro to use new Macro object
    Add editmacro to change a macro's response message
    Add setmacrocategory to change a macro's category
    Add renamemacro to rename a macro, while keeping its original message
    Add removemacros to remove entire categories of macros

--- src/main/kotlin/me/aberrantfox/hotbot/commandframework/commands/utility/MacroCommands.kt
+++ src/main/kotlin/me/aberrantfox/hotbot/commandframework/commands/utility/MacroCommands.kt
@@ -4,11 +4,13 @@ import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import me.aberrantfox.hotbot.commandframework.parsing.ArgumentType
+import me.aberrantfox.hotbot.dsls.command.Command
 import me.aberrantfox.hotbot.dsls.command.CommandSet
 import me.aberrantfox.hotbot.dsls.command.commands
 import me.aberrantfox.hotbot.dsls.embed.embed
 import me.aberrantfox.hotbot.services.CommandRecommender
 import me.aberrantfox.hotbot.services.configPath
+import java.awt.Color
 import java.io.File
 
 data class Macro(@SerializedName("name") val name: String,
@@ -22,41 +24,126 @@ val macros = loadMacroMap()
 fun macroCommands() =
     commands {
         command("addmacro") {
+            expect(ArgumentType.Word, ArgumentType.Word, ArgumentType.Sentence)
+            execute {
+                val name = (it.args.component1() as String).toLowerCase()
+                val category = (it.args.component2() as String).toLowerCase()
+                val message = it.args.component3() as String
+
+                if (it.container.has(name)) {
+                    it.respond("You dummy. There is a command with that name already...")
+                    return@execute
+                }
+
+                if (macros.any { it.name.toLowerCase() == name }) {
+                    it.respond("Yea... that macro exists...")
+                    return@execute
+                }
+
+                macros.add(Macro(name, message, category))
+                saveMacroMap(macros)
+
+                CommandRecommender.addPossibility(name)
+
+                it.safeRespond("**$name** (category: **$category**) will now respond with: **$message**")
+            }
+
+        }
+
+        command("editmacro") {
             expect(ArgumentType.Word, ArgumentType.Sentence)
             execute {
-                val key = (it.args[0] as String).toLowerCase()
+                val name = (it.args.component1() as String).toLowerCase()
+                val message = it.args.component2() as String
 
-                when {
-                    it.container.has(key) -> it.respond("You dummy. There is a command with that name already...")
+                val macro = macros.firstOrNull { it.name.toLowerCase() == name }
+                    ?: return@execute it.safeRespond("$name isn't a macro.")
 
-                    macroMap.containsKey(key) -> it.respond("Yea... that macro exists...")
+                macros.remove(macro)
+                macros.add(macro.copy(message=message))
 
-                    else -> {
-                        val value = it.message.contentRaw.substring("addmacro ".length + key.length + it.config.serverInformation.prefix.length + 1)
+                saveMacroMap(macros)
 
-                        macroMap[key] = value
-                        it.respond("**$key** will now respond with: **$value**")
+                it.safeRespond("**$name** (category: **${macro.category}**) will now respond with: **$message**")
+            }
+        }
 
-                        saveMacroMap(macroMap)
-                        CommandRecommender.addPossibility(key)
-                    }
-                }
+        command("setmacrocategory") {
+            expect(ArgumentType.Word, ArgumentType.Word)
+            execute {
+                val name = (it.args.component1() as String).toLowerCase()
+                val category = (it.args.component2() as String).toLowerCase()
+
+                val macro = macros.firstOrNull { it.name.toLowerCase() == name }
+                    ?: return@execute it.safeRespond("$name isn't a macro.")
+
+                macros.remove(macro)
+                macros.add(macro.copy(category=category))
+
+                saveMacroMap(macros)
+
+                it.safeRespond("**$name** category changed from **${macro.category}** to **$category**")
+            }
+        }
+
+        command("renamemacro") {
+            expect(ArgumentType.Word, ArgumentType.Word)
+            execute {
+                val oldName = (it.args.component1() as String).toLowerCase()
+                val newName = (it.args.component2() as String).toLowerCase()
+
+                val macro = macros.firstOrNull { it.name.toLowerCase() == oldName }
+                        ?: return@execute it.safeRespond("$oldName isn't a macro.")
+
+                macros.remove(macro)
+                macros.add(macro.copy(name=newName))
+
+                saveMacroMap(macros)
+
+                CommandRecommender.removePossibility(oldName)
+                CommandRecommender.addPossibility(newName)
+
+                it.safeRespond("**$oldName** renamed to **$newName**")
             }
         }
 
         command("removemacro") {
             expect(ArgumentType.Word)
             execute {
-                val key = (it.args[0] as String).toLowerCase()
+                val name = (it.args.component1() as String).toLowerCase()
 
-                if (macroMap.containsKey(key)) {
-                    macroMap.remove(key)
-                    saveMacroMap(macroMap)
-                    CommandRecommender.removePossibility(key)
-                    it.respond("$key - this macro is now gone.")
-                } else {
-                    it.respond("$key isn't a macro... ")
+                val macro = macros.firstOrNull { it.name.toLowerCase() == name }
+                        ?: return@execute it.safeRespond("$name isn't a macro.")
+
+                macros.remove(macro)
+                saveMacroMap(macros)
+
+                CommandRecommender.removePossibility(name)
+
+                it.safeRespond("$name - this macro is now gone.")
+            }
+        }
+
+        command("removemacros") {
+            expect(ArgumentType.Word)
+            execute {
+                val categoryName = (it.args.component1() as String).toLowerCase()
+
+                val toRemove = macros.filter { it.category.toLowerCase() == categoryName }
+
+                if (toRemove.isEmpty()) {
+                    it.safeRespond("$categoryName isn't the name of a known macro category.")
+                    return@execute
                 }
+
+                toRemove.forEach {
+                    macros.remove(it)
+                    CommandRecommender.removePossibility(it.name)
+                }
+
+                saveMacroMap(macros)
+
+                it.safeRespond("${toRemove.size} macros removed.")
             }
         }
 
