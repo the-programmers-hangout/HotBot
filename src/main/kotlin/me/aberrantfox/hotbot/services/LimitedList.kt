2bad9d8eaaa2cc7eed9f020d8ee9c7be4871fb59
moe <moe9999@protonmail.com>
2018-06-18 18:49:09 +0100

    Remove the first element in the list rather than size - 1

--- src/main/kotlin/me/aberrantfox/hotbot/services/LimitedList.kt
+++ src/main/kotlin/me/aberrantfox/hotbot/services/LimitedList.kt
@@ -4,7 +4,7 @@ package me.aberrantfox.hotbot.services
 class LimitedList<T>(val limit: Int) : ArrayList<T>() {
     override fun add(element: T): Boolean {
         if(size == limit) {
-            this.removeAt(size - 1)
+            this.removeAt(0)
         }
         return super.add(element)
     }
