e70c94586739fec72aecba861334bceae4e637f4
Amish Naidu <amhndu@gmail.com>
2018-09-14 11:39:05 +0530

    Use BanListener coupled with a timer to mark leave records as ban.
    This will use the Guild Ban event instead of checking the audit logs to
    find out if a user was banned, thus more rigid and now the timer has a
    higher value to ensure the ban event listener will only mark after the
    Guild Leave listener has been fired and record added.

---
+++ src/main/kotlin/me/aberrantfox/hotbot/listeners/BanListener.kt
@@ -0,0 +1,17 @@
+package me.aberrantfox.hotbot.listeners
+
+import com.google.common.eventbus.Subscribe
+import me.aberrantfox.hotbot.database.markLastRecordAsBan
+import net.dv8tion.jda.core.events.guild.GuildBanEvent
+import java.util.Timer
+import kotlin.concurrent.schedule
+
+class BanListener() {
+    @Subscribe
+    fun onGuildBan(event: GuildBanEvent) {
+        Timer().schedule(5 * 1000) {
+            println("Marking as banned")
+            markLastRecordAsBan(event.user.id, event.guild.id)
+        }
+    }
+}
--- src/main/kotlin/me/aberrantfox/hotbot/listeners/MemberListener.kt
+++ src/main/kotlin/me/aberrantfox/hotbot/listeners/MemberListener.kt
@@ -73,12 +73,7 @@ class MemberListener(val configuration: Configuration, val logger: BotLogger, va
         }
 
         val now = DateTime.now()
-        Timer().schedule(500) {
-            e.guild.auditLogs.type(ActionType.BAN).limit(3).queue { logs ->
-                val ban = logs.any { it.targetId == e.user.id && it.creationTime.toEpochSecond() - now.millis / 1000 in -2..2 }
-                insertLeave(e.user.id, DateTime(1000 * e.member.joinDate.toEpochSecond()), now, e.guild.id, ban)
-            }
-        }
+        insertLeave(e.user.id, DateTime(1000 * e.member.joinDate.toEpochSecond()), now, e.guild.id)
     }
 
     private fun buildJoinMessage(response: String, image: String, title: String) =
