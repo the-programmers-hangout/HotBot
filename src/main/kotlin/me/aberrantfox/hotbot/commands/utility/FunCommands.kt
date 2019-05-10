9e93b865030a321e558af50b532a770f0138974e
Jake Mattson <JakeJMattson@gmail.com>
2019-05-10 17:19:08 -0500

    Remove cowsay command

--- src/main/kotlin/me/aberrantfox/hotbot/commands/utility/FunCommands.kt
+++ src/main/kotlin/me/aberrantfox/hotbot/commands/utility/FunCommands.kt
@@ -1,6 +1,5 @@
 package me.aberrantfox.hotbot.commands.utility
 
-import com.github.ricksbrown.cowsay.Cowsay
 import me.aberrantfox.hotbot.services.Configuration
 import me.aberrantfox.kjdautils.api.dsl.*
 import me.aberrantfox.kjdautils.internal.command.arguments.*
@@ -39,25 +38,4 @@ fun funCommands(config: Configuration) =
                 it.respond(links.first().absUrl("href"))
             }
         }
-
-        command("cowsay") {
-            description = "Displays a cowsay with a given message. Run with no arguments to get a list of valid cows."
-            expect(arg(WordArg("Cow"), true, {""}), arg(SentenceArg("Message"), true, {""}))
-            execute {
-                val arg0 = it.args[0] as String
-                val arg1 = it.args[1] as String
-
-                it.respond(when {
-                    arg0.isBlank() && arg1.isBlank() -> CowsayData.validCows.joinToString (", ")
-                    arg1.isBlank() -> "```${Cowsay.say(arrayOf(arg0))}```"
-                    CowsayData.validCows.contains(arg0) -> "```${Cowsay.say(arrayOf("-f $arg0", arg1))}```"
-                    else -> "```${Cowsay.say(arrayOf("$arg0 $arg1"))}```"
-                })
-            }
-        }
-
     }
-
-object CowsayData {
-    val validCows = Cowsay.say(arrayOf("-l")).split("\n").filterNot { listOf("sodomized", "head-in", "telebears").contains(it) }
-}
