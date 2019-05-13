6616af94f5970f61de2e6a0067ba3c6f03e5ef49
Jake Mattson <JakeJMattson@gmail.com>
2019-05-13 13:40:21 -0500

    Create InitializerService
    Create a catch-all service to call the initialization code in order to remove this logic from main.

--- src/main/kotlin/me/aberrantfox/hotbot/MainApp.kt
+++ src/main/kotlin/me/aberrantfox/hotbot/MainApp.kt
@@ -1,14 +1,9 @@
 package me.aberrantfox.hotbot
 
-import me.aberrantfox.hotbot.arguments.LowerUserArg
-import me.aberrantfox.hotbot.commands.utility.*
-import me.aberrantfox.hotbot.database.*
-import me.aberrantfox.hotbot.optionallisteners.MentionListener
+import me.aberrantfox.hotbot.database.setupDatabaseSchema
 import me.aberrantfox.hotbot.services.*
-import me.aberrantfox.hotbot.utility.timeToDifference
 import me.aberrantfox.kjdautils.api.startBot
-import me.aberrantfox.kjdautils.internal.logging.*
-import net.dv8tion.jda.core.JDA
+import me.aberrantfox.kjdautils.internal.logging.convertChannels
 
 fun main(args: Array<String>) {
     val config = loadConfig() ?: return
@@ -19,43 +14,14 @@ fun main(args: Array<String>) {
 private fun start(config: Configuration) = startBot(config.serverInformation.token) {
     setupDatabaseSchema(config)
 
-    logger = convertChannels(config.logChannels, jda)
+    this.logger = convertChannels(config.logChannels, jda)
 
-    val manager = PermissionService(jda, config)
-
-    registerInjectionObject(config, container, logger, manager, this.config)
+    registerInjectionObject(config, container, logger, this.config)
 
     configure {
+        registerInjectionObject(this)
         prefix = config.serverInformation.prefix
         globalPath = "me.aberrantfox.hotbot"
         deleteMode = config.serverInformation.deletionMode
-        visibilityPredicate = { cmd, user, chan, _ -> manager.canUseCommand(user, cmd) && manager.canUseCommandInChannel(user, chan.id) }
-    }
-
-    LowerUserArg.manager = manager
-
-    setupMacroCommands(container, manager)
-
-    if (config.apiConfiguration.enableCleverBot) {
-        println("Enabling cleverbot integration.")
-        registerListeners(MentionListener(config, jda, manager))
-    }
-
-    manager.defaultAndPrunePermissions(container) // call this after loading all commands
-
-    loadPersistence(jda, logger, config)
-
-    logger.info("Fully setup, now ready for use.")
-}
-
-fun loadPersistence(jda: JDA, logger: BotLogger, config: Configuration) {
-    forEachIgnoredID {
-        config.security.ignoredIDs.add(it)
-    }
-
-    forEachReminder {
-        val difference = timeToDifference(it.remindTime)
-        val user = jda.getUserById(it.member)
-        scheduleReminder(user, it.message, difference, logger)
     }
 }
